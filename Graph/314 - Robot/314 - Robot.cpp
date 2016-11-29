d6efa542e33d7d2e7aa073b04e8787a4d1e05a3c
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2016-11-29 17:53:13 -0200

    work

---
+++ Graph/314 - Robot/314 - Robot.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, m, grafo[55][55];
+
+main()
+{
+  while(cin >> n >> m && (n+m))
+  {
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m ;j++)
+        cin >> matriz[i][j];
+
+    
+  }
+}
